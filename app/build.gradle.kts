9c94f5e348b862ee784acbf35e77fe1e3e5677b0
Reyvaldo Shiva Pramudya <reyvaldopro2003@gmail.com>
2024-11-19 10:25:15 +0700

    tambah glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+//    GLIDE
+    implementation(libs.glide)
 }
