ed6ee23230848689e65fb3f8fc32a8bc8fcb57bd
Farel <farelbayhaqi2544@gmail.com>
2024-11-19 22:55:17 +0700

    another layout

--- app/src/main/java/com/valdo/refind/ui/NewsFragment.kt
+++ app/src/main/java/com/valdo/refind/ui/NewsFragment.kt
@@ -5,64 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-<<<<<<< HEAD
 import com.valdo.refind.R
 
 class NewsFragment : Fragment() {
-=======
-import com.valdo.refind.ARG_PARAM1
-import com.valdo.refind.ARG_PARAM2
-import com.valdo.refind.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
->>>>>>> b34cb86e617592f55219b5b11ae3668c0209cb30
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false)
-    }
-<<<<<<< HEAD
 }
-
-=======
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NewsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
->>>>>>> b34cb86e617592f55219b5b11ae3668c0209cb30
