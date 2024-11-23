78832dff141cc7e6d95e88f9c0925c48d57777de
Reyvaldo Shiva Pramudya <reyvaldopro2003@gmail.com>
2024-11-23 14:23:43 +0700

    tambah scan,result,setting

--- app/src/main/java/com/valdo/refind/ui/BookmarkFragment.kt
+++ app/src/main/java/com/valdo/refind/ui/BookmarkFragment.kt
@@ -5,12 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.valdo.refind.databinding.FragmentBookmarkBinding
-import kotlinx.coroutines.launch
+import com.valdo.refind.R
 
 class BookmarkFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+    }
 }
--- app/src/main/java/com/valdo/refind/ui/NewsFragment.kt
+++ app/src/main/java/com/valdo/refind/ui/NewsFragment.kt
@@ -8,5 +8,9 @@ import androidx.fragment.app.Fragment
 import com.valdo.refind.R
 
 class NewsFragment : Fragment() {
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news, container, false)
+    }
 }
