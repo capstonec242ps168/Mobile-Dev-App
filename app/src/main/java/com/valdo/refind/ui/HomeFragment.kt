e3df51a170c3f46deeff9831a8fc6225b1316e43
Farel <farelbayhaqi2544@gmail.com>
2024-11-30 22:26:37 +0700

    craft button

--- app/src/main/java/com/valdo/refind/ui/HomeFragment.kt
+++ app/src/main/java/com/valdo/refind/ui/HomeFragment.kt
@@ -6,15 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.valdo.refind.R
 
 class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set click listeners for ImageButtons
+        view.findViewById<View>(R.id.btnPlastik).setOnClickListener {
+            openCraftFragment()
+        }
+
+        view.findViewById<View>(R.id.btnKertas).setOnClickListener {
+            openCraftFragment()
+        }
+
+        view.findViewById<View>(R.id.btnKardus).setOnClickListener {
+            openCraftFragment()
+        }
+
+        view.findViewById<View>(R.id.btnBaju).setOnClickListener {
+            openCraftFragment()
+        }
+
+        view.findViewById<View>(R.id.btnKaleng).setOnClickListener {
+            openCraftFragment()
+        }
+    }
+
+    private fun openCraftFragment() {
+        val craftFragment = CraftFragment()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, craftFragment)
+            .addToBackStack(null)
+            //transisi animasi
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 
     override fun onResume() {
