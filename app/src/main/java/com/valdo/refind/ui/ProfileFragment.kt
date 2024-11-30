0859bf4b6ed7b7a223e51863af686732afd431e5
Reyvaldo Shiva Pramudya <reyvaldopro2003@gmail.com>
2024-11-30 22:25:31 +0700

    clickable pencil in profile page

--- app/src/main/java/com/valdo/refind/ui/ProfileFragment.kt
+++ app/src/main/java/com/valdo/refind/ui/ProfileFragment.kt
@@ -1,12 +1,13 @@
 package com.valdo.refind.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.valdo.refind.R
 
 class ProfileFragment : Fragment() {
@@ -14,9 +15,22 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Hide Bottom Navigation Views
         activity?.findViewById<View>(R.id.bottomAppBar)?.visibility = View.GONE
         activity?.findViewById<View>(R.id.bottom_navigation)?.visibility = View.GONE
         activity?.findViewById<View>(R.id.fab)?.visibility = View.GONE
+
+        // Set Click Listeners for Icons
+        val editUsernameIcon = view.findViewById<ImageView>(R.id.edit_username_icon)
+        val editEmailIcon = view.findViewById<ImageView>(R.id.edit_email_icon)
+
+        editUsernameIcon.setOnClickListener {
+            navigateToEditProfile()
+        }
+
+        editEmailIcon.setOnClickListener {
+            navigateToEditProfile()
+        }
 
         setHasOptionsMenu(true)
     }
@@ -27,8 +41,15 @@ class ProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
-
     }
+
+    private fun navigateToEditProfile() {
+        val fragmentTransaction = parentFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, EditProfileFragment())
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
 
     override fun onPrepareOptionsMenu(menu: Menu) {
         super.onPrepareOptionsMenu(menu)
@@ -39,6 +60,7 @@ class ProfileFragment : Fragment() {
 
     override fun onDestroy() {
         super.onDestroy()
+        // Restore Bottom Navigation Views
         activity?.findViewById<View>(R.id.bottomAppBar)?.visibility = View.VISIBLE
         activity?.findViewById<View>(R.id.bottom_navigation)?.visibility = View.VISIBLE
         activity?.findViewById<View>(R.id.fab)?.visibility = View.VISIBLE
